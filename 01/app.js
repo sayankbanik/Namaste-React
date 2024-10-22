6e566a114f3d7b178c39d1ef381d9abd8fc5ed97
sayankbanik <39166919+sayankbanik@users.noreply.github.com>
2024-10-22 12:39:53 +0530

    createElement() understood

--- 01/app.js
+++ 01/app.js
@@ -1,5 +1,14 @@
-const head = document.createElement("h1");
-const divElement = document.getElementById("root");
-head.textContent = "Hello World from External JavaScript File";
-// console.log(divElement);
-divElement.appendChild(head);
+// create an element with React
+const heading = React.createElement(
+  "h1",
+  { id: "heading", sad: "lopo" },
+  "Hello World! from React."
+);
+// createElement("tag",{attributes},"tag content")
+console.log(heading); // object
+// createElement(type,props(attributes,children)) / returns a react element
+// create the root to manipulate/adding in the DOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// it's added to DOM, now we have to render it in the page
+root.render(heading);
+// render(object/reactElement) , it accepts the reactElement as an object and convert it to html tag
